26584aa3cb74a6445dfc708df6c9c121a8d3b53e
William Lorey <wwlorey@gmail.com>
2018-08-28 16:05:30 -0500

    Add solving verification function

--- new_types.py
+++ new_types.py
@@ -1,6 +1,9 @@
 import json
 
 
+ADJ_VALUE_DONT_CARE = 5
+
+
 class Coordinate:
   def __init__(self, x, y):
     self.x = x
@@ -9,6 +12,12 @@ class Coordinate:
   def __str__(self):
     """Returns an ordered pair in string form."""
     return '(' + str(self.x) + ', ' + str(self.y) + ')'
+  
+  def __eq__(self, other):
+    return self.x == other.x and self.y == other.y
+
+  def __hash__(self):
+    return (self.x + 1) * 100000 + self.y
 
 
 class Board:
@@ -37,6 +46,17 @@ class Board:
         for row in input_file:
           black_square_data = [int(i) for i in row.split()]
           self.black_squares[Coordinate(black_square_data[0] - 1, black_square_data[1] - 1)] = black_square_data[2]
+      
+    # Generate coordinate versions of the board
+    self.coord_board = []
+
+    for x in range(self.num_rows):
+      coord_list = []
+      for y in range(self.num_cols):
+        coord_list.append(Coordinate(x, y))
+      self.coord_board.append(coord_list)
+    
+    self.transpose_coord_board = [list(l) for l in zip(*self.coord_board)]
 
   def put_bulb(self, coord):
     """Attempts to place a bulb at coord position on the board.
@@ -96,12 +116,15 @@ class Board:
 
         for black_square in self.black_squares:
           if black_square.y == col and (upper_bound > black_square.x and lower_bound < black_square.x):
-              return True
+            return True
         
         return False # No black square delimiter was found
 
       # There is no shared row or column, or the coordinates are the same. Delimiting square is irrelevant
       return True
+
+    if coord in self.black_squares:
+      return False # Can't place a bulb on a black square 
 
     # Check for duplicate bulbs in rows & columns
     for bulb_coord in self.bulbs:
@@ -135,14 +158,80 @@ class Board:
     print()
   
   def check_solved(self):
+    """Checks to see if the board is solved.
 
+    Returns True if the following conditions are met:
+      1. Every non-black square is shined on by a bulb.
+      2. No bulbs shine on eachother.
+      3. Every black square has the required adjacent bulbs. (can be disabled using config file setting)
+    """
+
+    def get_adj_bulbs(coord):
+      """Returns the number of adjacent bulbs to coordinate coord"""
+      adj_coords = []
+      num_adj_bulbs = 0
+
+      if not coord.x == 0:
+        adj_coords.append(Coordinate(coord.x - 1, coord.y))
+
+      if not coord.x == self.num_rows - 1:
+        adj_coords.append(Coordinate(coord.x + 1, coord.y))
+      
+      if not coord.y == 0:
+        adj_coords.append(Coordinate(coord.x, coord.y - 1))
+      
+      if not coord.y == self.num_cols - 1:
+        adj_coords.append(Coordinate(coord.x, coord.y + 1))
+      
+      for coord in adj_coords:
+        if coord in self.bulbs:
+          num_adj_bulbs += 1
+      
+      return num_adj_bulbs
+
+    # Create and populate set of shined squares
+    shined_squares = set([])
+
+    for bulb_coord in self.bulbs:
+      # Create a list of adjacency lists - used for determining where the bulb shines
+      adj_coord_lists = []
+
+      adj_coord_lists.append(self.coord_board[bulb_coord.x][:bulb_coord.y][::-1])           # Row from this column to the left
+      adj_coord_lists.append(self.coord_board[bulb_coord.x][bulb_coord.y + 1:])             # Row from this column to the right
+      adj_coord_lists.append(self.transpose_coord_board[bulb_coord.y][:bulb_coord.x][::-1]) # Column from this row up
+      adj_coord_lists.append(self.transpose_coord_board[bulb_coord.y][bulb_coord.x + 1:])   # Column from this row down
+
+      for coord_list in adj_coord_lists:
+        for coord in coord_list:
+          if coord in self.black_squares:
+            break # Shine cannot propagate any further
+          elif coord in self.bulbs:
+            return False # Redundant check for bulb on bulb shining
+          else:
+            shined_squares.add(coord)
+    
+    # Verify all squares are accounted for
+    if (len(self.black_squares) + len(self.bulbs) + len(shined_squares)) != (self.num_cols * self.num_rows):
+      return False
+
+    # Check black square conditions
+    if self.config_settings["enforce_adj_quotas"] == "True":
+      for coord, adj_value in self.black_squares.items():
+        if adj_value < ADJ_VALUE_DONT_CARE and get_adj_bulbs(coord) != adj_value:
+          return False
+
+    return True
 
 
 b = Board()
+b.visualize()
 
 for x in range(b.num_rows):
   for y in range(b.num_cols):
     c = Coordinate(x, y)
-    b.put_bulb(c)
-    b.visualize()
+    print(b.put_bulb(c))
+
     
+b.visualize()
+
+print(b.check_solved())
